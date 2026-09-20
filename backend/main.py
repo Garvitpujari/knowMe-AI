2b4cbf3d78a6628089ec316d13d32e8085ed6a23
Garvit <garvitpujari98@gmail.com>
2026-09-21 01:01:39 +0530

    Fix Vercel FastAPI deployment

--- backend/main.py
+++ backend/main.py
@@ -3,23 +3,16 @@ from pydantic import BaseModel
 
 from rag import rag_chain
 
-
 app = FastAPI()
-
 
 class ChatRequest(BaseModel):
     question: str
-
 
 @app.get("/")
 def home():
     return {"message": "KnowMe AI backend is running"}
 
-
 @app.post("/chat")
 def chat(request: ChatRequest):
     answer = rag_chain.invoke(request.question)
-
-    return {
-        "answer": answer
-    }
+    return {"answer": answer}
