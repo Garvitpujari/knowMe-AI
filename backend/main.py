3417136930eb5a64caf0cec1a970fafeae96fb11
Garvit <garvitpujari98@gmail.com>
2026-09-21 20:10:28 +0530

    backend updated to allow req from 5173 port

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from rag import rag_chain
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 class ChatRequest(BaseModel):
     question: str
+
 
 @app.get("/")
 def home():
     return {"message": "KnowMe AI backend is running"}
+
 
 @app.post("/chat")
 def chat(request: ChatRequest):
